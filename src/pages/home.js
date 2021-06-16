670daa58b9249a6418b9222a18028a334034c9b4
unknown <jonaspng2000@gmail.com>
2021-06-16 16:01:41 +0800

    6th version

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,30 +1,41 @@
-import React, { useState,useEffect } from "react";
-import Navbar from "../components/navbar"
-
+import React from "react";
+import Navbar from "../components/navbar";
+import Carousel from "../components/carousel";
 
 function Home(){
+
+    let year= new Date().getFullYear();
+
     return(
         <div className="home-page">
-        <Navbar 
-            brand="/"
-            item1="Login"
-            link1="/login"
-            item2="Register"
-            link2="/register"
-            item3="Home"
-            link3="/"
-            logout="hidden"
-        />
-        <div className="home-content" style={{backgroundImage: "url(/Clean-Desk.jpg)"}}>
-            <div id="home-description" className="mx-auto">
-                <h1>Express ToDoList</h1>
-                <hr id="home-hr"></hr>
-                <p>A quick and easy solution to a digital to do list.</p>
-                <a className="btn btn-dark" href="/login">Login</a>
-                <a className="btn btn-light" href="/register">Register</a>
+            <Navbar 
+                brand="/"
+                item1="Login"
+                link1="/login"
+                item2="Register"
+                link2="/register"
+                item3="Home"
+                link3="/"
+                logout="hidden"
+            />
+            <div className="home-content" style={{backgroundImage: "url(/Clean-Desk.jpg)"}}>
+                <div id="home-description" className="mx-auto">
+                    <h1>Express ToDoList</h1>
+                    <hr id="home-hr"></hr>
+                    <p>A quick and easy solution to a digital to do list.</p>
+                    <a className="btn btn-dark" href="/login">Login</a>
+                    <a className="btn btn-light" href="/register">Register</a>
+                </div>          
             </div>
+            <section className="carousel-section">
+                <h1>How do I use Express ToDoList?</h1>
+                <Carousel/>
+            </section>
             
-        </div>
+            <footer>
+                <p>© Jonas Png, {year}</p>
+                <p>Background Photo by <a href="https://unsplash.com/@jessbaileydesigns?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Jess Bailey</a> on <a href="https://unsplash.com/s/photos/office-desk?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></p>
+            </footer>
         </div>
         
     );
