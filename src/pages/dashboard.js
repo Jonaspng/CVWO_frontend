eb0f5ebd5007739dd32a6c5d2209d54988885a32
Jonas <jonaspng2000@gmail.com>
2021-12-09 13:19:07 +0800

    resolving null category issue

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -151,7 +151,9 @@ function Dashboard(){
 
 
     function handleCategoryDeleteClick(event){
-        setCategoryConfirmation(window.confirm("Are you sure you want to delete the category? All list item in the category will also be deleted"));
+        useEffect(() => {
+            setCategoryConfirmation(window.confirm("Are you sure you want to delete the category? All list item in the category will also be deleted"));
+            }, []);
         console.log(categoryConfirmation);
         if (categoryConfirmation == true) {
             let id = event.currentTarget.value
