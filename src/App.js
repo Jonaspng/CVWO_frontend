036d0ef87ae799a93073c5b2dc166cbd99cf4dff
Jonas <jonaspng2000@gmail.com>
2021-12-21 15:36:06 +0800

    testing router

--- src/App.js
+++ src/App.js
@@ -17,63 +17,54 @@ function App(){
 
     useEffect(() => {updateAuth()}, []);
     console.log(auth)
-
-    function routes(){
-        // with new version of react-router-dom
-        // below is the new syntax
-        // <BrowserRouter>
-        //         <Routes>
-        //         <Route path="/" element={<App />} />
-        //         <Route path="/expenses" element={<Expenses />} />
-        //         <Route path="/invoices" element={<Invoices />} />
-        //     </Routes>
-        // </BrowserRouter>
-        if (auth){
-            return (
-                <Router>
-                    <Switch>
-                        <Route path = "/" exact>
-                            <Home />
-                        </Route>
-                        <Route path = "/login" exact>
-                            <Login />
-                        </Route>
-                        <Route path = "/register" exact>
-                            <Register />
-                        </Route>
-                        <Route path = "/dashboard" exact>
-                            <Dashboard />
-                        </Route>
-                        <Route path = "/profile" exact>
-                            <Profile />
-                        </Route>
-                        <Redirect to ="/dashboard"/>
-                    </Switch>
-                </Router>);
-        } else {
-            return (
-                <Router>
-                    <Switch>
-                        <Route path = "/" exact>
-                            <Home />
-                        </Route>
-                        <Route path = "/login" exact>
-                            <Login />
-                        </Route>
-                        <Route path = "/register" exact>
-                            <Register />
-                        </Route>
-                        <Redirect to ="/" />
-                    </Switch>
-           </Router>)
-        }
+    // with new version of react-router-dom
+    // below is the new syntax
+    // <BrowserRouter>
+    //         <Routes>
+    //         <Route path="/" element={<App />} />
+    //         <Route path="/expenses" element={<Expenses />} />
+    //         <Route path="/invoices" element={<Invoices />} />
+    //     </Routes>
+    // </BrowserRouter>
+    if (auth == "true"){
+        return (
+            <Router>
+                <Switch>
+                    <Route path = "/" exact>
+                        <Home />
+                    </Route>
+                    <Route path = "/login" exact>
+                        <Login />
+                    </Route>
+                    <Route path = "/register" exact>
+                        <Register />
+                    </Route>
+                    <Route path = "/dashboard" exact>
+                        <Dashboard />
+                    </Route>
+                    <Route path = "/profile" exact>
+                        <Profile />
+                    </Route>
+                    <Redirect to ="/dashboard"/>
+                </Switch>
+            </Router>);
+    } else {
+        return (
+            <Router>
+                <Switch>
+                    <Route path = "/" exact>
+                        <Home />
+                    </Route>
+                    <Route path = "/login" exact>
+                        <Login />
+                    </Route>
+                    <Route path = "/register" exact>
+                        <Register />
+                    </Route>
+                    <Redirect to ="/" />
+                </Switch>
+        </Router>)
     }
-
-    return(
-        // router paths
-        routes()
-       
-    );
 }
 
 export default App;
