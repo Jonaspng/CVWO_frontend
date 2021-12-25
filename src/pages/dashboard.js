0a4df1b271c561250a9bb9afafeb8c0b36af0a93
Jonas <jonaspng2000@gmail.com>
2021-12-25 22:27:04 +0800

    adding search functionality

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -284,7 +284,7 @@ function Dashboard(){
         fetch("https://todolist-backend-cvwo.herokuapp.com/users",{ credentials: 'include'})
             .then((res) => res.json())
             .then((username) => setUsername(username.user.username));
-    }, [categoryFilterValue, isInCategory, search]);
+    }, [listItem, categoryFilterValue, isInCategory, search]);
     
     // It checks with backend to see whether the user is logged in 
     // If auth == "false" the user will be redirected to the home page
